83a14d9fd78e72c5688a28fa8ac2052d66164319
vikas9dev <191975771+vikas9dev@users.noreply.github.com>
2026-01-18 05:38:11 +0000

    GitHub Actions with Docker

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi there, how are you?
-        </p>
+        <p>Docker Image with GitHub Actions</p>
         <a
           className="App-link"
           href="https://reactjs.org"
